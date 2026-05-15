cfaa91affb517aa6357baeace15b769fa65c19b8
郭逸轩 <mcymzc@qq.com>
2026-05-15 10:11:33 +0800

    add: 重写丢失文件

---
+++ src/main/java/com/mavis/model/admin/dto/WithdrawDetailVO.java
@@ -0,0 +1,21 @@
+package com.mavis.model.admin.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class WithdrawDetailVO {
+    private Long id;
+    private Long merchantId;
+    private BigDecimal amount;
+    private Integer status;
+    private String remark;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    private String merchantName;
+    private String alipayAccount;
+    private String nickName;
+    private String phone;
+}
